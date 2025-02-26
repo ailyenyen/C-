ce5d3ccf8dee6caef4b32deee72e58946b7f6127
Jan Mayen Mallen <157970742+ailyenyen@users.noreply.github.com>
2025-02-27 00:18:29 +0800

    Add files via upload

---
+++ ConcatInteger/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter Number 1: ");
+        String number1 = Console.ReadLine();
+        
+        Console.Write("Enter Number 2: ");
+        String number2 = Console.ReadLine();
+        
+        Console.Write("Enter Number 3: ");
+        String number3 = Console.ReadLine();
+        
+        Console.WriteLine(number1 + number2 + number3);
+    }
+}
